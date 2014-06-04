50ad9bf954a08bac7c426620894c8212a87e5b67
16SButtolph <16SButtolph@PRHS-132-10.prhs.sau48.k12.nh.us>
2014-06-04 12:38:33 -0400

    test

--- Minesweeper PRHS/src/GameFrame.java
+++ Minesweeper PRHS/src/GameFrame.java
@@ -32,7 +32,7 @@ public class GameFrame extends JFrame{
 	}
 
 	public static void main(String[] args) {
-		System.out.println("8===D");
+		System.out.println("");
 		initialize();
 	}
 	private static void initialize() {
